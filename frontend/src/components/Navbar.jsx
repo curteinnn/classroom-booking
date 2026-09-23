1f481ac5cf4d0c8f5c5d73eff1a24b488af67049
unknown <aldrydianreno212@gmail.com>
2026-09-23 14:16:51 +0700

    update-home

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,18 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
     <nav className="hidden absolute top-0 h-12 left-0 z-50 w-full bg-white px-8 md:block">
       <div className="flex h-12 items-center justify-between">
-        <Link to="/home" className="text-sm font-bold text-blue-400">
-          Classroom.
+        <Link to="/home" className="flex gap-2 justify-center items-center">
+          <img src={logo} alt="Classroom" className="h-8 w-auto" />
+          <h1 className="text-blue-400">
+            Smart<span className="text-blue-600">Class</span>
+          </h1>
         </Link>
 
-        <div className="flex items-center gap-8">
-          <Link
-            to="/home"
-            className="text-sm text-blue-400 transition hover:text-black"
-          >
-            Home
-          </Link>
-
+        <div className="flex items-center gap-8 ">
           <Link
             to="/rooms"
             className="text-sm text-blue-400 transition hover:text-black"
@@ -31,8 +28,7 @@ export default function Navbar() {
           </Link>
         </div>
 
-        {/* Profile */}
-        <div className="flex h-9 w-9 items-center justify-center rounded-full bg-blue-400 text-sm font-bold text-white">
+        <div className="flex h-9 w-9 bg-black items-center justify-center rounded-full text-sm font-bold text-white">
           A
         </div>
       </div>
